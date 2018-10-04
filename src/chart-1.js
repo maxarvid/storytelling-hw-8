299e80e5541b2ecaecb9a4cefaee33e7cc8f9062
Max <max.arvid.anderson@gmail.com>
2018-10-04 17:17:50 -0400

    chart1 data loaded

--- src/chart-1.js
+++ src/chart-1.js
@@ -8,6 +8,26 @@ var width = 780 - margin.left - margin.right
 // you'll need to read in the file. And you'll need
 // and svg, too, probably.
 
+var svg = d3
+  .select('#chart-1')
+  .append('svg')
+  .attr('height', height + margin.top + margin.bottom)
+  .attr('width', width + margin.left + margin.right)
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+var pie = d3.pie().value(function(d) {
+  return d.minutes
+})
+
+d3.csv(require('./data/time-breakdown.csv'))
+  .then(ready)
+  .catch(err => console.log('Failed with', err))
+
 function ready(datapoints) {
+  console.log(pie(datapoints))
+
+  var container = svg.append('g').attr('transform', `translate(${width/2},${height/2})`)
+
 
 }
