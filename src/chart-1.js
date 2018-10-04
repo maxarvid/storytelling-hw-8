b5cadc4ee5194ee1dfe664e7d90a72ad3160b894
Max <max.arvid.anderson@gmail.com>
2018-10-04 16:50:01 -0400

    pushing up raw files

---
+++ src/chart-1.js
@@ -0,0 +1,13 @@
+import * as d3 from 'd3'
+
+var margin = { top: 30, left: 30, right: 30, bottom: 30 }
+var height = 400 - margin.top - margin.bottom
+var width = 780 - margin.left - margin.right
+
+// At the very least you'll need scales, and
+// you'll need to read in the file. And you'll need
+// and svg, too, probably.
+
+function ready(datapoints) {
+
+}
