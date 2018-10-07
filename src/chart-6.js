85d248ad29dd72f0cfad005127d8e27c1eb8a917
Max <max.arvid.anderson@gmail.com>
2018-10-07 12:19:16 -0400

    fixed json parcel command

--- src/chart-6.js
+++ src/chart-6.js
@@ -10,12 +10,41 @@ let svg = d3
   .attr('height', height + margin.top + margin.bottom)
   .attr('width', width + margin.left + margin.right)
   .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+let radius = 200
+
+let radiusScale = d3
+  .scaleLinear()
+  .domain([0, 5])
+  .range([0, radius])
+
+var angleScale = d3.scaleBand().range([0, Math.PI * 2])
+
+var line = d3
+  .radialArea()
+  .innerRadius(0)
+  .outerRadius(d => radiusScale(d.score))
+  .angle(d => angleScale(d.category))
 
 d3.csv(require('./data/ratings.csv'))
   .then(ready)
   .catch(err => console.log('Failed with', err))
 
 function ready(datapoints) {
+  console.log('data is', datapoints)
+  var categoryList = datapoints.map(d => d.category)
+  angleScale.domain(categoryList)
 
+  var holder = svg
+    .append('g')
+    .attr('transform', `translate(${width / 2}, ${height / 2})`)
+
+  holder
+    .append('path')
+    .datum(datapoints)
+    .attr('d', line)
+    .attr('fill', 'rgb(255, 0, 0, 0.5')
+    .attr('stroke', 'none')
+    .attr('stroke-width', 2)
 }
